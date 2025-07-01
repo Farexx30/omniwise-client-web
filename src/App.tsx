9f0f8a7535d1889a6c80919a9817d7274f1efbb3
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-07-01 23:06:54 +0200

    Feature: Initial tailwindcss properties setup and utilizing them in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <div className="bg-main-page w-full h-screen bg-center bg-cover fixed inset-0 z-0" />
+
+      <div className="px-5 py-12 xs:p-10 max-w-7xl mx-auto flex flex-col relative z-10">
+        <header>
+          <h1><span className="text-gradient">Omniwise</span></h1>
+        </header>
+      </div>
+    </main>
   )
 }
 
