f5bf70e9dd2ec21ccde673c25eaddaf0d75b1a92
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-07-04 23:45:00 +0200

    Feature: Basic state management in LoginForm.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,21 +1,50 @@
+import { useState, type FormEvent } from "react";
+
 const LoginForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        //We will handle the api call here:
+        console.log("Email:", email);
+        console.log("Password:", password);
+    };
+
     return (
         <main>
             <div className="login-and-registration-container">
                 <div className="login-and-registration-form">
-                    <form action="" method="">
+                    <form onSubmit={handleSubmit}>
                         <h1>Sign in</h1>
                         <div className="input-box">
                             <label htmlFor="email">Email</label>
-                            <input id="email" type="text" placeholder="Email" required />
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="Email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className="input-box">
                             <label htmlFor="password">Password</label>
-                            <input id="password" type="password" placeholder="Password" required />
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="Password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
                         <button type="submit">Sign in</button>
                         <div className="mt-8">
-                            <a href="/register" className="text-gray-400 hover:underline"><p>Don't have an account? Create one</p></a>
+                            <a href="#" className="text-gray-400 hover:underline">
+                                <p>Don't have an account? Create one</p>
+                            </a>
                         </div>
                     </form>
                 </div>
