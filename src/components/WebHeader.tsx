2e59da11c7ec468e5105e7c0c8288f86afacef90
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-07-11 16:35:12 +0200

    Refactor: Clears localStorage data on logout.

--- src/components/WebHeader.tsx
+++ src/components/WebHeader.tsx
@@ -1,9 +1,30 @@
 import logo from '/logo.svg';
 import logoutIcon from '/logout.svg';
 import TransparentLink from './TransparentLink';
+import TransparentButton from './TransparentButton';
+import { useRouter } from '@tanstack/react-router';
+import { useQueryClient } from '@tanstack/react-query';
 
 
 const WebHeader = () => {
+    const router = useRouter();
+    const queryClient = useQueryClient();
+
+    const handleLogout = async() => {
+        localStorage.removeItem("tokenType");
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("expiresIn");
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("role");
+        localStorage.removeItem("currentCourseId");
+        localStorage.removeItem("currentCourseName");
+
+        await queryClient.cancelQueries();
+        queryClient.clear();
+
+        router.navigate({ to: "/login" })
+    }
+
     return (
         <div className="bg-transparent w-full h-8 flex items-center select-none justify-between p-5">
             <div className="flex items-center text-white font-bold">
@@ -11,10 +32,10 @@ const WebHeader = () => {
                 <span className="ml-2 font-light">Omniwise</span>
             </div>
             <div>
-                <TransparentLink
-                    to="/login"
+                <TransparentButton
                     iconSrc={logoutIcon}
                     text="Logout"
+                    onClick={handleLogout}
                 />
             </div>
         </div>
